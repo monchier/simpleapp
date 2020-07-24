ac3e5551b78edcc320050851f13108cbd6455fbe
Matteo <matteo@streamlit.io>
2020-07-24 10:54:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,4 @@ w2 = st.slider("Label 2", 0.0, 100.0, (25.0, 75.0), 0.5)
 st.write("Value 2:", w2)
 print("Matteo is writing something to standard output")
 import sys
-sys.stderr.write("spam\n")
-st.write("hello")
-import logging
-logging.info("info")
-logging.error("error")
+sys.stderr.write("spam5124\n")
