aadf2f766035237eee65123d117c0d0d9ef7d057
Matteo <matteo@streamlit.io>
2020-07-24 13:59:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ w2 = st.slider("Label 2", 0.0, 100.0, (25.0, 75.0), 0.5)
 st.write("Value 2:", w2)
 print("Matteo is writing something to standard output")
 import sys
-sys.stderr.write("somemagicsentence1234\n")
+sys.stderr.write("oddnumber\n")
 sys.stderr.flush()
